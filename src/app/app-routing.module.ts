ab4510b249a88209f32442d471f40ff665e621b1
lucas arcanjo <lucas.saolliveira@gmail.com>
2022-08-23 12:40:56 -0300

    adequacao tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnimaisModule } from './animais/animais.module';
 import { HomeModule } from './home/home.module';
 
 
@@ -11,7 +12,11 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: ()=> import('./home/home.module').then((m)=> HomeModule)
+    loadChildren: () => import('./home/home.module').then((m)=> m.HomeModule)
+  },
+  {
+    path: 'animais',
+    loadChildren: () => import('./animais/animais.module').then((m) => m.AnimaisModule)
   }
 ];
 
--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,28 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AutenticacaoService } from "src/app/autenticacao/autenticacao.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  usuario: string = "";
+  senha: string = "";
 
-  usuario: string = '';
-  senha: string = '';
+  constructor(
+    private authService: AutenticacaoService,
+    private router: Router
+  ) {}
 
-  constructor(private authService: AutenticacaoService ) { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   login() {
     this.authService.autenticar(this.usuario, this.senha).subscribe(() => {
-      console.log("Autenticado");
-    }, (error) => {
-      alert("Usuário ou senha inválido");
-      console.log(error);
-    })
+        this.router.navigate(['animais']);
+      }, (error) => {
+        alert("Usuário ou senha inválido");
+        console.log(error);
+      }
+    );
   }
-
 }
